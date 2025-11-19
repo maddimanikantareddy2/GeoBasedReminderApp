d00242c47b186f7a11ccf32de5be5c7ffad04a88
maddimanikantareddy2 <maddimanikantareddy2@gmail.com>
2025-11-20 00:23:21 +0530

    Added all required options in home screen for navigation

--- app/src/main/java/s3521330manikantareddy/teesproject/geobasedreminderapp/HomeActivity.kt
+++ app/src/main/java/s3521330manikantareddy/teesproject/geobasedreminderapp/HomeActivity.kt
@@ -1,13 +1,25 @@
 package s3521330manikantareddy.teesproject.geobasedreminderapp
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.runtime.Composable
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent {
+
+        }
     }
+}
+
+@Composable
+fun HomeScreen()
+{
+
 }
